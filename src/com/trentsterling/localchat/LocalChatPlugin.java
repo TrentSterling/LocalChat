d340ae2007eee4df5f31c9038058e18d5614a1c4
TrentSterling <trentster222@gmail.com>
2013-07-30 15:10:10 -0500

    Added permission nodes for global and admin chat
    Used to be ops only for admin chat, now there is a permission for admin
    chat, and a permission for global. Might add a local permission in the
    future. Still needs more refactoring!

--- src/com/trentsterling/localchat/LocalChatPlugin.java
+++ src/com/trentsterling/localchat/LocalChatPlugin.java
@@ -32,14 +32,13 @@ public class LocalChatPlugin extends JavaPlugin implements Listener
 				player.sendMessage(msg);
 			}
 		}
-
 	}
 
-	public void messageOps(String msg)
+	public void messageStaff(String msg)
 	{
 		for (Player p : Bukkit.getOnlinePlayers())
 		{
-			if (p.isOp())
+			if (p.isOp() || p.hasPermission("localchat.admin"))
 			{
 				p.sendMessage(msg);
 			}
@@ -49,36 +48,47 @@ public class LocalChatPlugin extends JavaPlugin implements Listener
 	@EventHandler
 	public void onChatMessage(AsyncPlayerChatEvent e)
 	{
-
 		if (e.isCancelled())
 		{
 			return;
 		}
 		Player player = e.getPlayer();
-		// messageOps("CHAT: <" + p.getDisplayName() + "> " + e.getMessage());
-
-		// Are we global or local? Lets find out!
-
 		String chatmode = String.valueOf(player.getMetadata("ChatMode").get(0).asString());
-
 		if (chatmode == "GLOBAL")
 		{
-			messageAllPlayers("[G]<" + player.getDisplayName() + "> " + e.getMessage());
-			getLogger().info("G: " + player.getName() + ": " + e.getMessage());
-		}
+			if (player.isOp() || player.hasPermission("localchat.global"))
+			{
+				messageAllPlayers("[G]<" + player.getDisplayName() + "> " + e.getMessage());
+				getLogger().info("G: " + player.getName() + ": " + e.getMessage());
+			}
+			else
+			{
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Global permissions needed, changing your chatmode!");
 
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is L(ocal)");
+				player.setMetadata("ChatMode", new FixedMetadataValue(this, "LOCAL"));
+			}
+		}
 		if (chatmode == "LOCAL")
 		{
-			messageLocal(player, "[Local]<" + player.getDisplayName() + "> " + e.getMessage());
+			messageLocal(player, "[Local]<" + player.getDisplayName() + "> " + ChatColor.GRAY + e.getMessage());
 			getLogger().info("L: " + player.getName() + ": " + e.getMessage());
 		}
-
 		if (chatmode == "ADMIN")
 		{
-			messageOps("[Admin]<" + player.getDisplayName() + "> " + ChatColor.GREEN + e.getMessage());
-			getLogger().info("A: " + player.getName() + ": " + e.getMessage());
-		}
+			if (player.isOp() || player.hasPermission("localchat.admin"))
+			{
+				messageStaff("[Admin]<" + player.getDisplayName() + "> " + ChatColor.GREEN + e.getMessage());
+				getLogger().info("A: " + player.getName() + ": " + e.getMessage());
+			}
+			else
+			{
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Admin permissions needed, changing your chatmode!");
 
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is L(ocal)");
+				player.setMetadata("ChatMode", new FixedMetadataValue(this, "LOCAL"));
+			}
+		}
 		e.setCancelled(true);
 
 	}
@@ -93,41 +103,43 @@ public class LocalChatPlugin extends JavaPlugin implements Listener
 		}
 
 		Player player = (Player) sender;
-		if (arg3.length == 0)
+
+		if (arg2.equalsIgnoreCase("L"))
 		{
-			player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Use /chat (G)lobal, (L)ocal, or (A)dmin to set your chat mode!");
+			player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is L(ocal)");
+			player.setMetadata("ChatMode", new FixedMetadataValue(this, "LOCAL"));
+			return true;
 		}
-		else
+
+		if (arg2.equalsIgnoreCase("G"))
 		{
-			String desiredMode = arg3[0];
-			if (desiredMode.equalsIgnoreCase("G"))
+			if (player.isOp() || player.hasPermission("localchat.global"))
 			{
 				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is G(lobal)");
 				player.setMetadata("ChatMode", new FixedMetadataValue(this, "GLOBAL"));
-			}
-			else if (desiredMode.equalsIgnoreCase("L"))
-			{
-				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is L(ocal)");
-				player.setMetadata("ChatMode", new FixedMetadataValue(this, "LOCAL"));
-			}
-			else if (desiredMode.equalsIgnoreCase("A"))
-			{
-
-				if (player.isOp())
-				{
-					player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is A(dmin)");
-					player.setMetadata("ChatMode", new FixedMetadataValue(this, "ADMIN"));
-				}
-				else
-				{
-					player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Sorry, OPs only, my friend.");
-				}
+				return true;
 			}
 			else
 			{
-				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Use /chat (G)lobal, (L)ocal, or (A)dmin to set your chat mode!");
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Global permissions needed");
 			}
+			return true;
 		}
+
+		if (arg2.equalsIgnoreCase("A"))
+		{
+			if (player.isOp() || player.hasPermission("localchat.admin"))
+			{
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Chat mode is A(dmin)");
+				player.setMetadata("ChatMode", new FixedMetadataValue(this, "ADMIN"));
+			}
+			else
+			{
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Admin permissions needed");
+			}
+			return true;
+		}
+		player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Use /G, /L, or /A to set your chat mode!");
 		return true;
 	}
 
